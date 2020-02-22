9f31c8c0bd354fd72a8289cb7d88d6e2533038b7
ege-yildirim <ege.yildirimm24@gmail.com>
2020-02-22 19:57:45 +0300

    Initial Commit

---
+++ Assets/Scripts/LegendaryCardRotate.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegendaryCardRotate : MonoBehaviour
+{
+    public GameObject heroCard;
+    public GameObject legendaryCard;
+    public GameObject legendrayCardImage;
+    public Material cardMaterial;
+    public float rotateSpeed=4f;
+    private float rotateDegree =360f;
+    public GameObject sparkle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sparkle.SetActive(false);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rotateSpeed -= 0.03f;
+        if (rotateSpeed > 0.03f)
+        {
+            RotateAround();
+
+        }
+
+
+        else if (rotateSpeed <= 0.03f)
+        {
+
+            rotateSpeed = 0;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation,  Quaternion.Euler(0,180,0),  Time.deltaTime);
+
+          
+          
+
+            legendrayCardImage.GetComponent<Renderer>().material = cardMaterial;
+            sparkle.SetActive(true);
+        }
+
+    }
+
+    
+
+
+    void RotateAround()
+    {
+
+       transform.Rotate(Vector3.up * rotateDegree * rotateSpeed * Time.deltaTime, Space.World);
+        
+
+    }
+
+   
+    
+}
---
+++ Assets/Scripts/chestClick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chestClick : MonoBehaviour
+  
+{
+    public GameObject openChest;
+    public GameObject closedChest;
+    public GameObject cardFrame;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        closedChest.SetActive(true);
+        openChest.SetActive(false);
+        cardFrame.SetActive(false);
+        
+    }
+
+    public void ClosedChestClick()
+    {
+        Debug.Log("Penis");
+        closedChest.SetActive(false);
+        openChest.SetActive(true);
+        cardFrame.SetActive(true);
+       
+        StartCoroutine(resetFrame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator resetFrame()
+    {
+
+        yield return new WaitForSeconds(5);
+        cardFrame.transform.rotation = Quaternion.Euler(180, 0, 180);
+
+    }
+}
